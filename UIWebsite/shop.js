efadc401e72021980695968a81f75d63eba241da
Isaac <isaacagutierrez10@yahoo.com>
2020-04-22 05:14:00 -0500

    removed old code, and put new progress into repo

---
+++ UIWebsite/shop.js
@@ -0,0 +1,24 @@
+var shop = angular.module('shop', []);
+
+shop.controller('ItemController', function ItemController($scope){
+  $scope.temp = 4;
+  $scope.images = {
+    "img1" : "./Assets/images/1.jpg",
+    "img2" : "./Assets/images/2.jpg",
+    "img3" : "./Assets/images/3.jpg",
+    "img4" : "./Assets/images/4.jpg",
+    "img5" : "./Assets/images/5.jpg",
+    "img6" : "./Assets/images/6.jpg",
+    "img7" : "./Assets/images/7.jpg",
+    "img8" : "./Assets/images/8.jpg",
+    "img9" : "./Assets/images/9.jpg",
+    "img10" : "./Assets/images/10.jpg",
+    "img11" : "./Assets/images/11.jpg",
+    "img12" : "./Assets/images/12.jpg",
+    "img13" : "./Assets/images/13.jpg",
+    "img14" : "./Assets/images/14.jpg",
+    "img15" : "./Assets/images/15.jpg",
+
+  }
+
+});
