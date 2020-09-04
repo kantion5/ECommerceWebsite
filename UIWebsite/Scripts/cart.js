540d91bdbdac501d0161c84073926594169b55b6
Isaac <isaacagutierrez10@yahoo.com>
2020-09-04 17:42:38 -0500

    Removing first iteration of the website

--- UIWebsite/Scripts/cart.js
+++
@@ -1,8 +0,0 @@
-var cart = angular.module('cart', []);
-
-cart.controller('CartController', function CartController($scope){
-  $scope.i = 10;
-  $scope.temp = [
-    {img1: '../Assets/images/1.jpg', price: 10, rating: 10}
-  ]
-});
--- UIWebsite/Scripts/productDetail.js
+++
@@ -1,5 +0,0 @@
-var product = angular.module('product', []);
-
-product.controller('ProductController', function ProductController($scope, $rootScope){
-  console.log($rootScope.item)
-});
